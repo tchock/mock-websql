c50c2e8c964bea42265b2c7ef86a24a6e4f8844a
Simon Jentsch <sjentsch@hyphe.me>
2015-02-20 03:45:27 +0100

    * comments added * TODOs added * items after a star in the SELECT statement will not be ignored anymore

--- websql.js
+++ websql.js
@@ -1,10 +1,12 @@
 // The data of the websql databases will be stored here!
 var mockSQLStorage = {};
 
+
 // openDatabase mock
 function mockOpenDatabase (name, version, displayName, estimatedSize, callback) {
   return new MockSQLDatabase(name, version, displayName, estimatedSize, callback);
 }
+
 
 // database object mock
 var MockSQLDatabase = function (name, version, displayName, estimatedSize, createCallback) {
@@ -51,24 +53,30 @@ MockSQLTransaction.prototype.executeSql = function (
   console.log(parsedSql);
   // SELECT statements
   if (parsedSql instanceof SQLParser.nodes.Select) {
-
     var tableData = mockSQLStorage[this.db._name][parsedSql.source.name.value];
     var limit = (parsedSql.limit === null) ? undefined : parsedSql.limit.value.value;
     var addedItem;
+    // iterate through table data
     for (var i = 0; i < tableData.length; i++) {
+      // only add to output data if no WHERE clause or it matches the WHERE clause
       if (!parsedSql.where || itemMatchesCondition(tableData[i], parsedSql.where.conditions)) {
         addedItem = {};
+        // iterate through all fields that should be selected
         for (var field in parsedSql.fields) {
           if (parsedSql.fields.hasOwnProperty(field)) {
+            // if star is used, add all fields to the addedItem
             if (parsedSql.fields[field] instanceof SQLParser.nodes.Star) {
+              // TODO use the table definition instead of the data object
               for (var key in tableData[i]) {
                 if (tableData[i].hasOwnProperty(key)) {
                   addedItem[key] = tableData[i][key];
                 }
               }
-              break;
+            // no star but actual field name stated
             } else {
               var fieldName = parsedSql.fields[field].field.value;
+              // TODO check for definition instead of data object property
+              // TODO error handling!
               if (tableData[i].hasOwnProperty(fieldName)) {
                 addedItem[fieldName] = tableData[i][fieldName];
               }
@@ -104,6 +112,8 @@ var MockSQLResultSet = function (data, rowsAffected, insertId) {
   this.rows = new MockSQLResultSetRowList(data);
 };
 
+
+// result set row list mock
 var MockSQLResultSetRowList = function (data) {
   this._data = data;
 };
@@ -111,6 +121,7 @@ var MockSQLResultSetRowList = function (data) {
 MockSQLResultSetRowList.prototype.item = function (index) {
   return this._data[index];
 };
+
 
 // Function to check if item matches the condition
 function itemMatchesCondition(item, condition) {
@@ -131,11 +142,13 @@ function itemMatchesCondition(item, condition) {
     right = condition.right;
   }
 
+  // return corresponding to the right operation
   switch(condition.operation) {
     case '=':
       return item[left.value] === right.value;
     case '!=':
       return item[left.value] !== right.value;
+    // TODO LIKE
     case 'AND':
       return left && right;
     case 'OR':
